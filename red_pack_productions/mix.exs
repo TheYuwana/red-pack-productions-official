5240b23f3a97e40982eec550b7a6418238d594e3
Jonathan van Putten <j.vanputten@yipyip.nl>
2017-09-27 16:41:08 +0200

    Set first version

--- red_pack_productions/mix.exs
+++ red_pack_productions/mix.exs
@@ -3,7 +3,7 @@ defmodule RedPackProductions.Mixfile do
 
   def project do
     [app: :red_pack_productions,
-     version: "0.0.1",
+     version: "1.0.0",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
