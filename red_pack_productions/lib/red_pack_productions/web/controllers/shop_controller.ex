fcbf36d95a3e056861db9ce4b9b288bf9a7d2d8b
Debbie <debbie.agnes@gmail.com>
2018-09-12 00:02:59 +0200

    shop detail page

--- red_pack_productions/lib/red_pack_productions/web/controllers/shop_controller.ex
+++ red_pack_productions/lib/red_pack_productions/web/controllers/shop_controller.ex
@@ -35,33 +35,38 @@ defmodule RedPackProductions.Web.ShopController do
   # ==================================
   def show(conn, %{"slug" => slug}) do
     
-    # Get all products
-    products = CachedContentful.Api.getEntriesByType("products", get_session(conn, :locale))
+    # # Get all products
+    # products = CachedContentful.Api.getEntriesByType("products", get_session(conn, :locale))
 
-    # Get product by slug
-    product = Enum.map(products, fn(product) -> 
-      if slug == product["fields"]["slug"] do
-        photo = CachedContentful.Api.getAssetById(product["fields"]["photo"]["sys"]["id"])["fields"]
-        sample_link = CachedContentful.Api.getAssetById(product["fields"]["sampleLink"]["sys"]["id"])["fields"]
-        %{
-          title: product["fields"]["title"],
-          subtitle: product["fields"]["subtitle"],
-          slug: product["fields"]["slug"],
-          old_price: product["fields"]["oldPrice"],
-          new_price: product["fields"]["newPrice"],
-          description: product["fields"]["description"],
-          sample_link: sample_link["file"]["url"],
-          photo: photo["file"]["url"]
-        }
-      end
-    end)
-      |> Enum.filter(fn(x) -> x != nil end)
-      |> Enum.fetch!(0)
+    # # Get product by slug
+    # product = Enum.map(products, fn(product) -> 
+    #   if slug == product["fields"]["slug"] do
+    #     photo = CachedContentful.Api.getAssetById(product["fields"]["photo"]["sys"]["id"])["fields"]
+    #     sample_link = CachedContentful.Api.getAssetById(product["fields"]["sampleLink"]["sys"]["id"])["fields"]
+    #     %{
+    #       title: product["fields"]["title"],
+    #       subtitle: product["fields"]["subtitle"],
+    #       slug: product["fields"]["slug"],
+    #       old_price: product["fields"]["oldPrice"],
+    #       new_price: product["fields"]["newPrice"],
+    #       description: product["fields"]["description"],
+    #       sample_link: sample_link["file"]["url"],
+    #       photo: photo["file"]["url"]
+    #     }
+    #   end
+    # end)
+    #   |> Enum.filter(fn(x) -> x != nil end)
+    #   |> Enum.fetch!(0)
+
+    # conn
+    #   |> assign(:og_description, "Low budget/HIGH QUALITY Audio-Recording studio.")
+    #   |> assign(:title, "Red Pack Productions - Shop - #{product.title}")
+    #   |> render("show.html", product: product)
 
     conn
       |> assign(:og_description, "Low budget/HIGH QUALITY Audio-Recording studio.")
-      |> assign(:title, "Red Pack Productions - Shop - #{product.title}")
-      |> render("show.html", product: product)
+      |> assign(:title, "Red Pack Productions - Shop - test")
+      |> render("show.html")
   end
 
   def test(conn, _) do
