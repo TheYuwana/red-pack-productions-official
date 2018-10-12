1d670dacdc64d853f066422f747b4220cba6b19e
root <debbie.agnes@gmail.com>
2018-10-12 13:28:31 +0000

    added js for basket

--- red_pack_productions/assets/js/main.js
+++ red_pack_productions/assets/js/main.js
@@ -319,3 +319,105 @@ $(function() {
     }
   });
 });
+
+
+// SHOPPING BASKET
+$(function() {
+
+  // Set scroll for basket
+  $(window).scroll(function(){
+    if($(window).scrollTop() === 0){
+      $(".basket-container").removeClass("basket-scrolling");
+    }else{
+      if(!$(".basket-container").hasClass("basket-scrolling")){
+        $(".basket-container").toggleClass("basket-scrolling");
+      }
+    }
+  });
+  
+  // Set Icon open and close
+  $(".basket-icon").click(function(){
+		$(this).parent().toggleClass("open-basket");
+  });
+  
+  // Set add to basket events
+  $(".basket-add").click(function(){
+   sb_add_to_basket(
+    $(this).data("basket-product-id"),
+    $(this).data("basket-product-name"),
+    $(this).data("basket-product-price")
+   ); 
+  });
+  
+  // Set remove product from basket
+  $(".remove-product").click(function(){
+    sb_remove_from_basket($(this));
+  });
+  
+  // Set on change for prouct amounts
+  $(".basket-products input").change(function(){
+    sb_sum_total();
+  });
+	
+});
+
+
+
+function sb_add_to_basket(pid, name, price){
+  if(sb_product_not_exist(pid)){
+    var shortName = name;
+    if(name.length > 10){
+      shortName = name.substring(15, 0) + "...";
+    }
+    $(".basket-products ul").append(
+      $("<li>").append(
+        $("<span>", {"class": "oi oi-x remove-product"}).click(function(){
+          sb_remove_from_basket($(this));
+        }),
+        $("<input>", {"type": "number", "min": "1"}).val(1).change(function(){
+          sb_sum_total();
+        }),
+        shortName,
+        $("<span>", {"class": "amount"}).text("\u20AC " + price)
+      ).data("price", price).data("pid", pid)
+    );
+  }
+  sb_sum_total();
+  sb_update_basket_amount();
+}
+
+function sb_product_not_exist(pid){
+  var notFound = true;
+  $(".basket-products ul").find("li").each(function(){
+    if($(this).data("pid") == pid){
+      var val = Number($(this).find("input").val()) + 1;
+      $(this).find("input").val(val);
+      notFound = false;
+      return false;
+    }else{
+      notFound = true;
+    }
+  });
+  return notFound;
+}
+
+function sb_remove_from_basket(product){
+  $(product).parent().remove();
+  sb_sum_total();
+  sb_update_basket_amount();
+}
+
+function sb_sum_total(){
+  var total = 0;
+  $(".basket-products ul").find("li").each(function(){
+    var amount = Number($(this).find("input").val());
+    total = total + (amount * Number($(this).data("price")));
+  });
+  $(".basket-total-amount").text("\u20AC " + total);
+}
+
+function sb_update_basket_amount(){
+  $(".basket-count p").text($(".basket-products ul").find("li").length);
+}
+
+
